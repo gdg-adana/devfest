f7f6544de73b526c8df058ddd2d3def9618b4a2c
bugragoksu <bugragoksu2@gmail.com>
2022-10-16 23:04:33 +0300

    add get_it

---
+++ lib/injection.dart
@@ -0,0 +1,8 @@
+import 'package:devfest/injection.config.dart';
+import 'package:get_it/get_it.dart';
+import 'package:injectable/injectable.dart';
+
+final getIt = GetIt.instance;
+
+@InjectableInit()
+void configureDependencies() => $initGetIt(getIt);
